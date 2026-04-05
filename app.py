1d8e0876e58e93f2794e0a0992f0d819f5d87b81
mathewdillian02 <mathewdillian02@gmail.com>
2026-04-05 22:49:29 +0000

    fix: removed duplicate reply token call

--- app.py
+++ app.py
@@ -121,7 +121,7 @@ def handle_message(event):
    # line_bot_api.reply_message(
        # event.reply_token,
        # TextSendMessage(text=reply_text)
-    )
+    
 
 @app.route("/", methods=['GET'])
 def home():
